9071e5ac48122edc261d40fd537853bfd2630d1f
metalelf0@baldur <metalelf0@baldur>
2009-07-15 09:45:28 +0200

    Added find_by_year_month method

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,12 @@
 class Expense < ActiveRecord::Base
 	validates_presence_of :description
 	validates_numericality_of :amount
+	
+
+	def Expense.find_by_year_month year, month
+		start_date = Date.parse("#{year}/#{month}/01")
+		end_date = (start_date >> 1) - 1
+		puts Expense.count.to_s + " expenses"
+		Expense.find(:all).select { |expense| expense.date.between?(start_date, end_date) }
+	end
 end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -2,11 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Expense do
   before(:each) do
+    Expense.destroy_all
     @valid_attributes = {
     	:description => "Sample expense",
     	:amount => "100.0"
     }
   end
+
+	after(:each) do
+		
+	end
 
   it "should create a new instance given valid attributes" do
     Expense.create!(@valid_attributes)
@@ -18,10 +23,18 @@ describe Expense do
   	e.errors_on(:description).should_not be_nil
   end
   
-  it "should not be valid with strange amounts" do
+  it "should not be valid with non numerical amounts" do
   	e = Expense.new(:description => "Sample description")
   	e.amount = "Gnagno"
   	e.should_not be_valid
+  end
+  
+  it "should retrieve expenses in a given month" do
+  	e1 = Expense.create!(:description => "First", :amount => 0, :date => Date.parse("2009/01/01"))
+  	e2 = Expense.create!(:description => "Second", :amount => 10, :date => Date.parse("2009/02/01"))
+  	e3 = Expense.create!(:description => "Third", :amount => 20, :date => Date.parse("2009/03/01"))
+  	expenses_of_february = Expense.find_by_year_month(2009, 02)
+  	expenses_of_february.size.should eql(1)
   end
   
 end
