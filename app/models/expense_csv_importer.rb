6f988e018d2fb43a764bb86b2c609495da17f3cd
metalelf0@baldur <metalelf0@baldur>
2009-07-20 19:22:40 +0200

    Completed csv importer

---
+++ app/models/expense_csv_importer.rb
@@ -0,0 +1,24 @@
+require 'fastercsv'
+require 'pp'
+
+class ExpenseCsvImporter
+
+  attr_accessor :path
+
+  def initialize path
+    @path = path
+  end
+
+  def import_line line
+    attributes = FasterCSV::parse(line)[0]
+    Expense.create(:date => attributes[0], :amount => attributes[1],
+      :description => attributes[2], :category => attributes[3])
+  end
+  
+  def import
+    File.read(@path).each do |line|
+      import_line line
+    end
+  end
+
+end
---
+++ spec/models/expense_csv_importer_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'fastercsv'
+
+describe ExpenseCsvImporter do
+
+  fixtures_path = "spec/fixtures/expenses.csv"
+
+  before(:each) do
+    Expense.delete_all
+  end
+  
+  it "should load a csv file" do
+    csv = File.open(fixtures_path)
+    lines = 0
+    csv.each { |l| lines += 1; }
+    lines.should eql(40)
+    csv.close
+  end
+  
+  it "should parse a line from the csv file" do
+    Expense.count.should eql(0)
+    csv_line = File.open(fixtures_path).first
+    importer = ExpenseCsvImporter.new(fixtures_path)
+    importer.import_line csv_line
+    Expense.count.should eql(1)
+  end
+  
+  it "should parse all the lines from the csv file" do
+    Expense.count.should eql(0)
+    importer = ExpenseCsvImporter.new fixtures_path
+    importer.import
+    Expense.count.should eql(40)
+  end
+
+end
