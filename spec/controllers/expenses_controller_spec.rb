071aa3125a2b141dd1853b2badafa6ac56f6a3eb
Andrea Schiavini <metalelf0@gmail.com>
2009-07-23 08:07:03 +0200

    Started adding total row

--- spec/controllers/expenses_controller_spec.rb
+++ spec/controllers/expenses_controller_spec.rb
@@ -8,20 +8,21 @@ describe ExpensesController do
  	
  		before(:each) do
  		  Expense.delete_all 			
- 			e1 = mock_model(Expense, :description => "First", :amount => 0, :date => Date.parse("2009/01/01"), :category => "",
+ 			@e1 = mock_model(Expense, :description => "First", :amount => 0, :date => Date.parse("2009/01/01"), :category => "",
  			:bancomat => true, :ufficio => true, :buoni => 0)
-  		e2 = mock_model(Expense, :description => "Second", :amount => 10, :date => Date.parse("2009/02/01"), :category => "",
+  		@e2 = mock_model(Expense, :description => "Second", :amount => 10, :date => Date.parse("2009/02/01"), :category => "",
  			:bancomat => true, :ufficio => true, :buoni => 0)
-  		e3 = mock_model(Expense,:description => "Third", :amount => 20, :date => Date.parse("2009/03/01"), :category => "",
+  		@e3 = mock_model(Expense,:description => "Third", :amount => 20, :date => Date.parse("2009/03/01"), :category => "",
  			:bancomat => true, :ufficio => true, :buoni => 0)
  			Expense.stub(:categories).and_return(["Altro"])
- 			Expense.stub(:all).and_return([e1, e2, e3])
- 			Expense.stub(:find_by_year_month).and_return([e1])
+ 			Expense.stub(:all).and_return([@e1, @e2, @e3])
+ 			Expense.stub(:find_by_year_month).and_return([@e1])
  		end
  	
- 		it "should receive a success response" do
+ 		it "should receive a success response and show all the entries" do
 		  get :index
-		  @response.should be_success
+		  response.should be_success
+		  response.should have_tag('tr.expense-row', :count => 1)
 		end
 		
 		it "should show only the entries for a given month, if params are given" do
@@ -35,6 +36,15 @@ describe ExpensesController do
       response.should be_success
       response.should have_tag('tr.insert_rowA', :count => 5)
       response.should have_tag('tr.insert_rowB', :count => 5)      
+    end
+    
+    it "should show a line with the totals" do
+      Expense.stub(:find_by_year_month).and_return([@e1, @e2, @e3])
+      
+      get :index
+      response.should be_success
+      response.should have_tag('tr.totals')
+      response.should have_tag('td.total-amount', :text => "30")
     end
  
 	end
