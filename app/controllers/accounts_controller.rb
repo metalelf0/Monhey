127998496cb4efc9a540a4d64aa2cce3e3f50bd9
Andrea Schiavini <metalelf0@gmail.com>
2009-09-03 08:18:28 +0200

    Small fixes, mostly layout

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,29 +1,21 @@
 class AccountsController < ApplicationController
-  # GET /accounts
-  # GET /accounts.xml
+
   def index
     @accounts = Account.all
   end
 
-  # GET /accounts/1
-  # GET /accounts/1.xml
   def show
     @account = Account.find(params[:id])
   end
 
-  # GET /accounts/new
-  # GET /accounts/new.xml
   def new
     @account = Account.new
   end
 
-  # GET /accounts/1/edit
   def edit
     @account = Account.find(params[:id])
   end
 
-  # POST /accounts
-  # POST /accounts.xml
   def create
     @account = Account.new(params[:account])
     if @account.save
@@ -34,8 +26,6 @@ class AccountsController < ApplicationController
     end  
   end
 
-  # PUT /accounts/1
-  # PUT /accounts/1.xml
   def update
     @account = Account.find(params[:id])
     if @account.update_attributes(params[:account])
@@ -46,8 +36,6 @@ class AccountsController < ApplicationController
     end
   end
 
-  # DELETE /accounts/1
-  # DELETE /accounts/1.xml
   def destroy
     @account = Account.find(params[:id])
     @account.destroy
