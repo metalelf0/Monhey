63c3fcce66e5c2417d5ddddbf547d9e863c25fe9
Andrea Schiavini <metalelf0@gmail.com>
2010-04-27 08:40:44 +0200

    Added easymoney csv importer

--- app/models/expense_csv_importer.rb
+++ app/models/expense_csv_importer.rb
@@ -4,21 +4,47 @@ require 'pp'
 class ExpenseCsvImporter
 
   attr_accessor :path
+  attr_accessor :mode
 
   def initialize path, mode=:standard
     @path = path
-    
+    configure_for mode
   end
 
   def import_row parsed_row
-    Expense.create(:date => parsed_row[0], :amount => parsed_row[1],
-      :description => parsed_row[2], :category => Category.find_or_create_by_name(parsed_row[3]))
+    Expense.create(:date => Date.parse(parsed_row[ @fields[:date] ]), 
+                   :amount => parsed_row[ @fields[:amount] ],
+                   :description => parsed_row[ @fields[:description] ], 
+                   :category => Category.find_or_create_by_name(parsed_row[ @fields[:category] ]))
   end
   
   def import
-    FasterCSV.foreach(@path) do |parsed_row|
+    FasterCSV.foreach(@path, {:col_sep => @col_sep}) do |parsed_row|
       import_row parsed_row
     end
+  end
+
+  private
+  
+  def configure_for mode
+    if mode == :standard
+      @fields = {
+        :date => 0,
+        :amount => 1,
+        :description => 2,
+        :category => 3
+      }
+      @col_sep = ','
+    elsif mode == :easymoney
+      @fields = {
+        :date => 4,
+        :amount => 2,
+        :description => 0,
+        :category => 1
+      }
+      @col_sep = '|'
+    end
+    
   end
 
 end
--- spec/models/expense_csv_importer_spec.rb
+++ spec/models/expense_csv_importer_spec.rb
@@ -11,20 +11,22 @@ describe ExpenseCsvImporter do
     parsed_row = ["2009-06-21","-15","Pranzo terme primia","Cibo"]
     category = mock_model(Category)
     Category.should_receive(:find_or_create_by_name).with("Cibo").and_return(category)
-    Expense.should_receive(:create).with(:date => "2009-06-21", :amount => "-15",
+    Date.should_receive(:parse).with("2009-06-21").and_return(:some_date)
+    Expense.should_receive(:create).with(:date => :some_date, :amount => "-15",
       :description => "Pranzo terme primia", :category => category)
     importer.import_row parsed_row
   end
  
-  # it "should parse a line from the easymoney csv file" do
-  #   importer = ExpenseCsvImporter.new("a path", :easymoney)
-  #   csv_line = '"Benza"|"Transportation"|-60.68|"Uncleared"|"15 Mar 2010"|""|""'
-  #   category = mock("Category")
-  #   Category.should_receive(:find_or_create_by_name).with("Transportation").and_return(category)
-  #   Expense.should_receive(:create).with(:date => "2010-03-15", :amount => "-60.68",
-  #     :description => "Benza", :category => category)
-  #   importer.import_row csv_line
-  # end
+  it "should parse a line from the easymoney csv file" do
+    importer = ExpenseCsvImporter.new("a path", :easymoney)
+    parsed_row = ["Benza", "Transportation", "-60.68", "Uncleared", "15 Mar 2010"]
+    category = mock_model(Category)
+    Date.should_receive(:parse).with("15 Mar 2010").and_return(:some_date)
+    Category.should_receive(:find_or_create_by_name).with("Transportation").and_return(category)
+    Expense.should_receive(:create).with(:date => :some_date, :amount => "-60.68",
+      :description => "Benza", :category => category)
+    importer.import_row parsed_row
+  end
   
   
   it "should parse all the lines from the csv file" do
@@ -32,7 +34,13 @@ describe ExpenseCsvImporter do
     Category.should_receive(:find_or_create_by_name).exactly(40).times
     Expense.should_receive(:create).exactly(40).times
     importer.import
+  end
 
+  it "should parse all the lines from the easymoney csv file" do
+    importer = ExpenseCsvImporter.new fixtures_path_easymoney, :easymoney
+    Category.should_receive(:find_or_create_by_name).exactly(8).times
+    Expense.should_receive(:create).exactly(8).times
+    importer.import
   end
 
 end
