6319ae2c2e5993463a4f9a23b19f3d36a8bc1a14
Andrea Schiavini <metalelf0@gmail.com>
2013-05-19 21:19:57 +0200

    Adding users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,17 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    render :text => auth_hash.inspect
-  end
+ 
+    @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    if @authorization
+      render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
+    else
+      user = User.new :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]["email"]
+      user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+      user.save
+ 
+      render :text => "Hi #{user.name}! You've signed up."
+    end  end
 
   def failure
   end
