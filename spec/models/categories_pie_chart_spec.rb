91d2da0f392f5cac23ff04cce75a7134d0c67b2b
Andrea Schiavini <andrea.schiavini@Yggdrasil.local>
2013-10-06 12:15:01 +0200

    Fixed categories_pie_chart_spec

--- spec/models/categories_pie_chart_spec.rb
+++ spec/models/categories_pie_chart_spec.rb
@@ -9,8 +9,7 @@ describe CategoriesPieChart do
 
   subject { CategoriesPieChart.new(:user => user, :date => date) } 
 
-  its(:monthly_categories_report) { should == { "Food" => -10 }  }
-  its(:url) { should ==  'http://chart.apis.google.com/chart?cht=p&chd=t:10&chs=350x150&chl=Food&chco=FF0000' }
+  its(:monthly_categories_report) { should == {"Food"=>{:amount=>-10.0, :color=>"#111"}}  }
 
 end
 
