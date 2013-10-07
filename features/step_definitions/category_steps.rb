293400894c30cf8836abe117d1a612cfdcc4faec
Andrea Schiavini <andrea.schiavini@Yggdrasil.local>
2013-10-07 12:34:21 +0200

    Removed useless features dir

--- features/step_definitions/category_steps.rb
+++
@@ -1,9 +0,0 @@
-When /^I visit detail page for category "([^\"]*)"$/ do |category_name|
-  cat = Category.find_by_name(category_name)
-  visit category_url(cat)
-end
-
-Given /^I have a category named "([^\"]*)"$/ do |category_name|
-  Factory(:category, :name => category_name)
-end
-
