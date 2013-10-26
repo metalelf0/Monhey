23f6c23be0e54c528374246a2ee3e3878532c6c8
Andrea Schiavini <metalelf0@gmail.com>
2013-10-26 21:14:57 +0200

    Removed left-for-month and wage

--- spec/integration/expense_repository_spec.rb
+++ spec/integration/expense_repository_spec.rb
@@ -33,10 +33,6 @@ describe ExpenseRepository do
     john.expenses_by_year_and_month(:date => Date.new(2012, 1, 1)).should == [ john_expense, another_john_expense ]
   end
 
-  it "calculates how much money is left for a given month" do
-    john.left_for_month(:date => Date.new(2012, 1, 1), :wage => 2000).should == 2000 + -10 + -10
-  end
-
   it "searches expenses by year, month, and category" do
     john.expenses_by_year_month_and_category(:date => Date.new(2012, 1, 1), :category_name => "Food").should == [ john_expense ]
   end
@@ -48,24 +44,6 @@ describe ExpenseRepository do
   it "calculates daily average for a given month" do
     john.daily_average_for_month(:date => Date.new(2012, 1, 1)).should == (-20.0 / 31)
   end
-  
-  context "Calculating monthly prevision" do
-
-    context "when current month" do
-
-      it "returns the current average multiplied the number of days in current month" do
-        average = john.daily_average_for_month(:date => Date.today)
-        john.prevision_for_month(:date => Date.today).should == average * 31
-      end
-      
-      it "can also include wage" do
-        average = john.daily_average_for_month(:date => Date.today)
-        john.prevision_for_month(:date => Date.today, :wage => 1000).should == 1000 + average * 31
-      end
-          
-    end
-  
-  end  
 
   context "Daily total" do
 
